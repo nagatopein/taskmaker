cfa9ee6b28860cd3c3279704ddf82df5d42a9053
Taj <betetat94@gmail.com>
2022-06-01 16:12:59 -0400

    added data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -236,60 +236,21 @@ var saveTasks = function () {
 
 var loadTasks = function () {
     // load tasks from local storage
-    let tasks = (localStorage.getItem("tasks")); // wrap in JSON.parse
+    var savedTasks = (localStorage.getItem("tasks")); // wrap in JSON.parse
     // console.log(tasks);
 
-    if (!tasks) {
-        tasks = [];
+    if (!savedTasks) {
         return false;
     }
 
-    tasks = JSON.parse(tasks);
-    console.log(tasks);
+    savedTasks = JSON.parse(savedTasks);
+    // console.log(tasks);
 
-    for (var i = 0; i < tasks.length; i++) {
-        if (tasks[i].id = taskIdCounter);
-        // console.log(tasks[i]);
-        // console.log(tasks[i].id);
-
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", (tasks[i].id));
-        // console.log(listItemEl);
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        console.log(taskInfoEl);
-
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-
-        listItemEl.appendChild(taskActionsEl);
-        console.log(listItemEl);
-
-        if (tasks[i].status === "to do") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === "complete") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-
-        taskIdCounter++;
-
-
-        console.log(listItemEl);
-
-
-
-    };
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the createTaskEl() function
+        createTaskEl(savedTasks[i]);
+    }
 };
 
 
@@ -311,6 +272,51 @@ loadTasks();
 
 
 /* FROM loadTasks() */
+
+
+// for (var i = 0; i < tasks.length; i++) {
+//     if (tasks[i].id = taskIdCounter);
+//     // console.log(tasks[i]);
+//     // console.log(tasks[i].id);
+
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.setAttribute("data-task-id", (tasks[i].id));
+//     // console.log(listItemEl);
+
+//     var taskInfoEl = document.createElement("div");
+//     taskInfoEl.className = "task-info";
+//     taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+//     console.log(taskInfoEl);
+
+//     listItemEl.appendChild(taskInfoEl);
+
+//     var taskActionsEl = createTaskActions(tasks[i].id);
+
+//     listItemEl.appendChild(taskActionsEl);
+//     console.log(listItemEl);
+
+//     if (tasks[i].status === "to do") {
+//         listItemEl.querySelector("select[name='status-change']").selectedIndex = 0
+//         tasksToDoEl.appendChild(listItemEl);
+//     }
+//     else if (tasks[i].status === "in progress") {
+//         listItemEl.querySelector("select[name='status-change']").selectedIndex = 1
+//         tasksInProgressEl.appendChild(listItemEl);
+//     }
+//     else if (tasks[i].status === "complete") {
+//         listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
+//         tasksCompletedEl.appendChild(listItemEl);
+//     }
+
+//     taskIdCounter++;
+
+
+//     console.log(listItemEl);
+
+
+
+// };
     // if (console.log(tasks[i])) {
     //     tasks[i].id = taskIdCounter;
     //     console.log(tasks[i]);
